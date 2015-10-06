74daf31e398b5e58981792a2e4bb65ef465b60dd
Melanie Palmer <mpalmer.design@gmail.com>
2015-10-06 10:41:00 -0600

    lab4

---
+++ lab4/CMakeFiles/ast_tree.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/melaniepalmer/Documents/College/Senior Year/Cake Whispers/compilers-labs/lab4/ast.cpp" "/Users/melaniepalmer/Documents/College/Senior Year/Cake Whispers/compilers-labs/lab4/CMakeFiles/ast_tree.dir/ast.cpp.o"
+  "/Users/melaniepalmer/Documents/College/Senior Year/Cake Whispers/compilers-labs/lab4/driver.cpp" "/Users/melaniepalmer/Documents/College/Senior Year/Cake Whispers/compilers-labs/lab4/CMakeFiles/ast_tree.dir/driver.cpp.o"
+  "/Users/melaniepalmer/Documents/College/Senior Year/Cake Whispers/compilers-labs/lab4/evaluate.cpp" "/Users/melaniepalmer/Documents/College/Senior Year/Cake Whispers/compilers-labs/lab4/CMakeFiles/ast_tree.dir/evaluate.cpp.o"
+  "/Users/melaniepalmer/Documents/College/Senior Year/Cake Whispers/compilers-labs/lab4/prettyPrint.cpp" "/Users/melaniepalmer/Documents/College/Senior Year/Cake Whispers/compilers-labs/lab4/CMakeFiles/ast_tree.dir/prettyPrint.cpp.o"
+  "/Users/melaniepalmer/Documents/College/Senior Year/Cake Whispers/compilers-labs/lab4/printExpressions.cpp" "/Users/melaniepalmer/Documents/College/Senior Year/Cake Whispers/compilers-labs/lab4/CMakeFiles/ast_tree.dir/printExpressions.cpp.o"
+  "/Users/melaniepalmer/Documents/College/Senior Year/Cake Whispers/compilers-labs/lab4/visitor.cpp" "/Users/melaniepalmer/Documents/College/Senior Year/Cake Whispers/compilers-labs/lab4/CMakeFiles/ast_tree.dir/visitor.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ lab4/CMakeFiles/ast_tree.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ast_tree.dir/ast.cpp.o"
+  "CMakeFiles/ast_tree.dir/driver.cpp.o"
+  "CMakeFiles/ast_tree.dir/evaluate.cpp.o"
+  "CMakeFiles/ast_tree.dir/prettyPrint.cpp.o"
+  "CMakeFiles/ast_tree.dir/printExpressions.cpp.o"
+  "CMakeFiles/ast_tree.dir/visitor.cpp.o"
+  "ast_tree.pdb"
+  "ast_tree"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ast_tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
